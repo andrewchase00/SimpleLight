8f58443b0cf41ad14fc27450715b3490cc1f5b75
andrewchase00 <andrewchase00@gmail.com>
2015-02-06 18:23:13 -0800

    cleaned and commented LightFragment

--- app/src/main/java/com/chaspe/simplelight/LightFragment.java
+++ app/src/main/java/com/chaspe/simplelight/LightFragment.java
@@ -1,23 +1,12 @@
 package com.chaspe.simplelight;
 
-import android.annotation.TargetApi;
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.FragmentManager;
-import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.hardware.Camera;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.os.IBinder;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,177 +14,138 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+
+/**
+                                                                                                    * Created by Andrew Chase on 2/3/2015.
+                                                                                                    * Simple Light's main Fragment. Contains the view and all UI functions
+                                                                                                    * based on parameters set up by LightService.
+ */
 
 
 public class LightFragment extends Fragment {
-    public static final String TAG = "LightFragment";
 
-
-
-    public static final String EXTRA_CHANGE = "CHANGE";
+    public static final String EXTRA_CHANGE = "CHANGE";                                             // Tags for extras put onto the Intent used to launch LightService
     public static final String EXTRA_STROBE = "STROBE";
     public static final String EXTRA_FREQUENCY = "FREQUENCY";
 
-    private CheckBox mLightButton;
+    private Intent mIntent;                                                                         // Template LightService Intent that extras can be added to
+
+    private CheckBox mLightButton;                                                                  // UI Elements
     private Button mExitButton;
     private SeekBar mBar;
-    private Toolbar mToolbar;
     private CheckBox mBox;
-    private ActionBar mActionBar;
 
-
-    private boolean mStrobe;
+    private boolean mStrobe;                                                                        // Light Parameters
     private int mFrequency;
-    private Intent mIntent;
 
-
-    LightService mService;
-    boolean mBound = false;
-
+    LightService mService;                                                                          // LightService used to initialize light parameters
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
 
-            mIntent = new Intent();
-            mIntent.setAction("com.chaspe.simplelight.LightService");
+        mIntent = new Intent();                                                                     // Initializing template LightService Intent
+        mIntent.setAction("com.chaspe.simplelight.LightService");
 
-            getActivity().getApplicationContext().bindService(mIntent, mConnection, getActivity().getApplicationContext().BIND_AUTO_CREATE);
-
-
+        getActivity().getApplicationContext().bindService(mIntent,                                  // Bind Light Service to initialize light parameters
+                mConnection,
+                Context.BIND_AUTO_CREATE);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_light, container, false);
+        View v = inflater.inflate(R.layout.fragment_light, container, false);                       // Inflate the layout for this fragment
 
-
-
-       mToolbar = (Toolbar) v.findViewById(R.id.toolbar);
-
-
-
-        mExitButton = (Button) v.findViewById(R.id.exit);
+        mExitButton = (Button) v.findViewById(R.id.exit);                                           // Set up on Click Listener for Exit Button
         mExitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LightActivity.ACTION_CLOSE);
+                Intent intent = new Intent(LightActivity.ACTION_CLOSE);                             // Broadcast Close intent
                 getActivity().sendBroadcast(intent);
             }
         });
 
-        mBox = (CheckBox) v.findViewById(R.id.checkbox);
-        mBox.setChecked(mStrobe);
+        mBox = (CheckBox) v.findViewById(R.id.checkbox);                                            // Initialize Check Box to mStrobe and start on Checked Change Listener
+        mBox.setChecked(mStrobe);                                                                   // for Strobe Check Box
         mBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                mBar.setEnabled(isChecked);
 
-                mStrobe = isChecked;
+                mBar.setEnabled(isChecked);                                                         // Enable/disable Frequency Seek Bar
 
-                if(isChecked){
+                mStrobe = isChecked;                                                                // Update Strobe parameter
+
+                if(isChecked){                                                                      // Update Strobe Check Box Text Color
                     mBox.setTextColor(getResources().getColor(R.color.a400));
                 } else {
                     mBox.setTextColor(getResources().getColor(R.color.t2));
                 }
 
-                updateService(false);
+                updateService(false);                                                               // Update LightService with new light parameters
             }
         });
 
-
-        mLightButton = (CheckBox) v.findViewById(R.id.light_button);
+        mLightButton = (CheckBox) v.findViewById(R.id.light_button);                                // Set up on Click Listener for Light Button
         mLightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateService(true);
+                updateService(true);                                                                // Tell LightService to switch light on/off
             }
         });
 
-        mBar = (SeekBar) v.findViewById(R.id.brightness);
-        mBar.setEnabled(mStrobe);
-
+        mBar = (SeekBar) v.findViewById(R.id.brightness);                                           // Initialize frequency Seek Bar based on light parameters
+        mBar.setEnabled(mStrobe);                                                                   // and set on Progress Changed Listener
         mBar.setProgress(mFrequency);
         mBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.d(TAG, "Seek bar at " + progress);
-                mFrequency = progress;
-                updateService(false);
+                mFrequency = progress;                                                              // Update Frequency parameter
+                updateService(false);                                                               // Update LightService with new light parameters
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
-        return v;
+        return v;                                                                                   // Submit View
     }
 
-
-    public void updateService(boolean change){
-
-            Intent intent = mIntent;
+    public void updateService(boolean change){                                                      // Function used to send an intent to LightService with light
+            Intent intent = mIntent;                                                                // parameters and whether or not to change flash state
             intent.putExtra(LightFragment.EXTRA_CHANGE, change);
             intent.putExtra(LightFragment.EXTRA_STROBE, mStrobe);
             intent.putExtra(LightFragment.EXTRA_FREQUENCY, mFrequency);
             getActivity().getApplicationContext().startService(mIntent);
     }
 
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-
-
-    }
-
-
-
-    private ServiceConnection mConnection = new ServiceConnection() {
-
+    private ServiceConnection mConnection = new ServiceConnection() {                               // Connection used to temporarily bind to LightService
+                                                                                                    // to initialize parameters
         @Override
         public void onServiceConnected(ComponentName className,
                                        IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            LightService.LightBinder binder = (LightService.LightBinder) service;
+            LightService.LightBinder binder = (LightService.LightBinder) service;                   // Get an instance of the bound LightService
             mService = binder.getService();
 
-            mStrobe = mService.mStrobe;
+            mStrobe = mService.mStrobe;                                                             // Update light parameters and UI
             mFrequency = mService.mFrequency;
-            boolean lightOn = mService.wasOn;
-
             mBar.setProgress(mFrequency);
             mBox.setChecked(mStrobe);
-
             mLightButton.setChecked(mService.mOn);
-            getActivity().getApplicationContext().unbindService(mConnection);
 
-            //if(lightOn) {
-            //    updateService(true);
-            //}
+            getActivity().getApplicationContext().unbindService(mConnection);                       // Unbind service after update
         }
 
         @Override
         public void onServiceDisconnected(ComponentName arg0) {
-
-
         }
     };
-
-
 }
 
